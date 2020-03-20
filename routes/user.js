44b60ce3c6f8355008e101cfa87003b4a4c63425
Subba <Subba@gmail.com>
2020-03-19 22:36:54 -0500

    changes user.js

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,9 @@ const Router = express.Router();
 const mysqlConnection = require("../connection");
 
 Router.get("/",(req, res)=>{
-    mysqlConnection.query("SELECT * from user;",(err, rows, fields)=>{
+    mysqlConnection.query("select * from user;",(err, rows, fields)=>{
         if(!err){
-            res.send(rows);
+            res.send(rows[0].first_name);
         }else{
             console.log(err);
         }
